cb582da8c6c57dc374146b7920c1f8fbe34c3406
Gabriel <gabrielstertz2012@gmail.com>
2022-11-30 09:04:29 -0300

    add upload avatar and cors

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,13 +1,20 @@
 const {Router} = require("express");
+const multer = require("multer");
+const uploadConfig = require("../configs/upload");
 
 const UsersController = require('../controllers/usersController');
+const UserAvatarController = require("../controllers/userAvatarController");
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
 
 const usersRoutes = Router();
 
 const usersController = new UsersController();
+const userAvatarController = new UserAvatarController();
+const upload = multer(uploadConfig.MULTER);
+
 usersRoutes.post('/', usersController.create);
 usersRoutes.put('/', ensureAuthenticated, usersController.update);
+usersRoutes.patch('/avatar', ensureAuthenticated, upload.single("avatar"), userAvatarController.update);
 
 
 module.exports = usersRoutes;
